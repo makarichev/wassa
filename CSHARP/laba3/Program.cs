ed6a0244ac9cd0b41d4b1a29439995b3c64e1cde
dmitrij.makarichev@gmail.com <dmitrij.makarichev@gmail.com>
2021-11-07 14:25:24 +0300

    asdfasdf

---
+++ CSHARP/laba3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
